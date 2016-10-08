f24446fa17e8eb51b70030b136c5aa00a72ff27c
remozhang <814895077@qq.com>
2016-10-08 22:07:41 +0800

    finish task3.2
    哇哈哈哈

--- task0002/js/util.js
+++ task0002/js/util.js
@@ -282,3 +282,75 @@ function getPosition(element) {
         y: y
     }
 }
+
+——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+// task 3.2
+// 实现一个简单的Query
+function $(selector) {
+    var ele = document;
+    var sele = selector.replace(/\s+/, ' ').split(' ');    // 去除多余的空格并分割
+
+    for (var i = 0, len = sele.length; i < len; i++) {
+
+        switch (sele[i][0]) {    // 从子节点中查找
+            case '#':
+                ele = ele.getElementById(sele[i].substring(1));
+                break;
+            case '.':
+                ele = ele.getElementsByClassName(sele[i].substring(1))[0];
+                break;
+            case '[':
+                var valueLoc = sele[i].indexOf('=');
+                var temp = ele.getElementsByTagName('*');
+                var tLen = temp.length;
+                if (valueLoc !== -1) {
+                    var key = sele[i].substring(1, valueLoc);
+                    var value = sele[i].substring(valueLoc + 1, sele[i].length - 1);
+                    for (var j = 0; j < tLen; j++) {
+                        if (temp[j][key] === value) {
+                            ele = temp[j];
+                            break;
+                        }
+                    }
+                }
+                else {
+                    var key = sele[i].substring(1, sele[i].length - 1);
+                    for (var j = 0; j < tLen; j++) {
+                        if (temp[j][key]) {
+                            ele = temp[j];
+                            break;
+                        }
+                    }
+                }
+                break;
+            default :
+                ele = ele.getElementsByTagName(sele[i])[0];
+                break;
+        }
+    }
+
+    if (!ele) {
+        ele = null;
+    }
+
+    return ele;
+}
+/*
+// 可以通过id获取DOM对象，通过#标示，例如
+$("#adom"); // 返回id为adom的DOM对象
+
+// 可以通过tagName获取DOM对象，例如
+$("a"); // 返回第一个<a>对象
+
+// 可以通过样式名称获取DOM对象，例如
+$(".classa"); // 返回第一个样式定义包含classa的对象
+
+// 可以通过attribute匹配获取DOM对象，例如
+$("[data-log]"); // 返回第一个包含属性data-log的对象
+
+$("[data-time=2015]"); // 返回第一个包含属性data-time且值为2015的对象
+
+// 可以通过简单的组合提高查询便利性，例如
+$("#adom .classa"); // 返回id为adom的DOM所包含的所有子节点中，第一个样式定义包含classa的对象
+*/
